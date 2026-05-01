8da9824981102315bda9b09ec2f1cac3dce812b9
Fatih Kalemci <fatihkalemci96@gmail.com>
2026-05-02 01:12:51 +0300

    Add button for changing grid size

--- script.js
+++ script.js
@@ -1,16 +1,38 @@
-let container = document.querySelector(".pad-container");
+const container = document.querySelector(".pad-container");
 
-let padSide = 64;
+const redrawButton = document.querySelector("#redraw");
+redrawButton.addEventListener('click', () => {
+  let newGridSize = 16
+  do {
+    newGridSize = prompt("Enter new grid size (max: 100)");
+  } while (newGridSize > 100 || newGridSize <= 0 || isNaN(newGridSize) || Math.floor(+newGridSize) !== +newGridSize)
+  clearGrid();
+  setGrid(+newGridSize);
+})
 
-let padPixelNumber = padSide * padSide;
+let padSide = 16;
 
-for (let i = 0; i < padPixelNumber; i++) {
-    let div = document.createElement("div");
+function setGrid(sidePixels){
+  let padPixelNumber = sidePixels * sidePixels;
+
+  for (let i = 0; i < padPixelNumber; i++) {
+    const div = document.createElement("div");
     div.classList.add("pixel");
-    div.style.flex = `0 0 ${100/padSide}%`;
+    div.style.flex = `0 0 ${100/sidePixels}%`;
     div.addEventListener('mouseenter', () => {
         div.style.backgroundColor = "black"
     });
     container.appendChild(div);
+  }
 }
+
+function clearGrid() {
+  while (container.firstChild) {
+    container.removeChild(container.lastChild);
+  }
+}
+
+
+
+setGrid(padSide)
 
